8fbf6c6cc056ef66d6781859955c974600a0327e
HansGjedsig <132069951+HansGjedsig@users.noreply.github.com>
2023-04-28 15:26:25 +0200

    Add files via upload

--- hacathon.py
+++ hacathon.py
@@ -9,8 +9,8 @@ ds2 = nc.Dataset('temp_anomaly_ex2.nc')
 ds3 = nc.Dataset('temp_anomaly_ex3.nc')
 
 ex1 = True
-ex2 = False
-ex3 = False
+ex2 = True
+ex3 = True
 
 test = np.array(ds1.variables['tempanomaly'][1419,:,:])
 
@@ -44,13 +44,18 @@ if ex2:
     for i in range(13):
         Tempdata = np.array(ds2.variables['tempanomaly'][1419-i,:,:])
         print(i)
-        for x in range(4,6):
+        for x in range(5,7):
             for y in range(4,6):
                 dist = np.array(1/np.sqrt((X-lats[y])**2+(Y-longs[x])**2)**4)
-                dist[4:6,4:6]=0
+                dist[5:7,4:6]=0
                 distweight = dist/dist.sum()
                 Tempdata[x,y]=(Tempdata*distweight).sum()
         predictions.append(Tempdata)
+    data_CSV = []
+    for i in range(13):
+        data_CSV.append(predictions[12-i][5,5])
+    df= pd.DataFrame(data_CSV)
+    df.to_csv("ex2.csv",index=False)
 
 if ex3:
     lats = ds3.variables['lat'][:]
@@ -68,6 +73,11 @@ if ex3:
                 distweight = dist/dist.sum()
                 Tempdata[x,y]=(Tempdata*distweight).sum()
         predictions.append(Tempdata)
+    data_CSV = []
+    for i in range(13):
+        data_CSV.append(predictions[12-i][5,5])
+    df= pd.DataFrame(data_CSV)
+    df.to_csv("ex3.csv",index=False)
     
 
 
